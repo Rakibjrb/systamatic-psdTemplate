0c5a85eff3f702e92522cb0b2d83fbf7477a0024
Rakibjrb <78774179+Rakibjrb@users.noreply.github.com>
2022-10-13 21:35:41 +0600

    Add files via upload
    systamatic psd full template converted into html file full source code are here

---
+++ systamatice html-css/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.main_header').meanmenu({
+        meanMenuContainer: '#mobile_menu',
+        meanScreenWidth: 820,
+    });
+});
+
+AOS.init({
+    offset: 400,
+});
